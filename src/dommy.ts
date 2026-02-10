575de4bb5c117543e8897785b9889a4db1f7f12d
melissacrawford396 <melissacrawford396@gmail.com>
2026-02-09 19:15:54 -0600

    reading about other access patterns in DOM

--- src/dommy.ts
+++ src/dommy.ts
@@ -5,8 +5,14 @@ const app = document.getElementById("app");
 // 2. Create a new <p></p> element programmatically
 const p = document.createElement("p");
 
+
 // 3. Add the text content
 p.textContent = "I am the Dommy Mommy!";
 
 // 4. Append the p element to the div element
 app?.appendChild(p);
+
+//For those interested, you can interact with custom tag elements using the
+// document.getElementsByTagName
+const customTags = document.getElementsByTagName("taggy");
+console.log(customTags);
